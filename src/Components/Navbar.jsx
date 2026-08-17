99c2b4744b32ed325ca0c0880e7f3b3f2ffb3f75
HamphreyChinyerere <hamphreytanatswa@gmail.com>
2026-08-17 09:58:13 +0200

    Navbar Update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
-import './Navbar.css'
 
 function Navbar() {
   return (
-    <div className='nvabar'>
-      <img src="#" alt="Logo.png" />
-      <ul className='links'>
-        <li><a href="Home">Home</a></li>
-        < li><a href="Products">Products</a></li>
-        <li><a href="About">About</a></li>
-        <li><a href="Contact">Contact</a></li>
-        <li><a href="Login">Login</a></li>
-        <li><a href="Signup">Signup</a></li>
-        </ul>
-    </div>
+    <nav className='navbar'>
+
+        <div className="navba-logo">
+            a
+        </div>
+
+    </nav>
   )
 }
 
